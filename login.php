9122574d5f4b3b6b75c70210b08c824e6eb5ff79
Evans Osumba <osumbaevanzz@gmail.com>
2025-04-16 18:56:56 +0300

    improved UI of login and register pages

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
+    <link href="register-login.css" rel="stylesheet">
     <style>
 
     </style>
@@ -84,7 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <main>
         <div class="form-card">
-            <h2>Welcome Back!</h2>
+            <h2 style="color: linear-gradient(135deg, #6c5ce7, #00cec9)";>Welcome Back!</h2>
             <?php if ($alert): ?>
                 <div class="alert alert-danger">
                     <i class="fas fa-exclamation-circle"></i>
